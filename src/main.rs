6ecfbd7c5a000a70ce6c8cffcdc1a679a9017a39
Hector <agenthector007@gmail.com>
2025-06-02 18:47:15 +0200

    print joystick input

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
+mod print;
+
+use arduino_hal::adc::AdcSettings;
+use print::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
+
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut adc = arduino_hal::Adc::new(dp.ADC, AdcSettings::default());
 
-    let mut led = pins.d13.into_output();
+    let joystick_pin_x = pins.a1.into_analog_input(&mut adc);
+    let joystick_pin_y = pins.a2.into_analog_input(&mut adc);
+
+    let serial = arduino_hal::default_serial!(dp, pins, 57600);
+    
+    // Set up global console for use in macros
+    avr_device::interrupt::free(|cs| {
+        CONSOLE.borrow(cs).replace(Some(serial));
+    });
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(3000);
+        let x = joystick_pin_x.analog_read(&mut adc);
+        let y = joystick_pin_y.analog_read(&mut adc);
+
+        println!("X: {}, Y: {}", x, y);
     }
 }
